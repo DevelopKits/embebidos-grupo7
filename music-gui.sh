f872035b4e943d960edf0aa272c66983112f316e
fabian <fsolano.13@gmail.com>
2013-10-04 18:03:33 -0600

    Agregada las opciones de start y stop

--- music-gui.sh
+++ music-gui.sh
@@ -6,17 +6,32 @@ PIDFILE="/var/run/music-gui.pid"
 
 test -x "$musicgui" || exit 0
 
-if [ -e $PIDFILE ]; then
-      PIDDIR=/proc/$(cat $PIDFILE)
-      if [ -d ${PIDDIR} -a  "$(readlink -f ${PIDDIR}/exe)" = "${matrixgui}" ]; then
-        echo "$DESC already started; not starting."
-      else
-        echo "Removing stale PID file $PIDFILE."
-        rm -f $PIDFILE
-      fi
-fi
+case "$1" in
+  start)
+    if [ -e $PIDFILE ]; then
+        PIDDIR=/proc/$(cat $PIDFILE)
+        if [ -d ${PIDDIR} -a  "$(readlink -f ${PIDDIR}/exe)" = "${matrixgui}" ]; then
+          echo "$DESC already started; not starting."
+        else
+          echo "Removing stale PID file $PIDFILE."
+          rm -f $PIDFILE
+        fi
+    fi
 
-echo -n "Starting Music GUI application"
-start-stop-daemon --start --quiet --background -m --pidfile $PIDFILE --exec $musicgui -- $GUI_OPTS 
-echo "."
+    echo -n "Starting Music GUI application"
+    start-stop-daemon --start --quiet --background -m --pidfile $PIDFILE --exec $musicgui -- $GUI_OPTS 
+    echo "."
+    ;;
+   
+   stop)
+    echo -n "Stopping Matrix GUI application"
+    start-stop-daemon --stop --quiet --pidfile $PIDFILE
+    echo "."
+    ;;
+    
+   *)
+    echo -n "Usage: /etc/init.d/music-gui {start|stop}"
+    exit 1
+esac    
+
 exit 0
